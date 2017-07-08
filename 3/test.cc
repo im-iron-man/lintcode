7e21289a3b9dddd1b1ae52d71af86185c266ffcb
gaoxinge <gaoxx5@gmail.com>
2017-07-08 16:43:04 +0800

    3

---
+++ 3/test.cc
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    /*
+     * param k : As description.
+     * param n : As description.
+     * return: How many k's between 0 and n.
+     */
+    int digitCounts(int k, int n) {
+        int count = 0;
+        if (!k) ++count;
+        for (int i = 1; i < n+1; ++i) {
+            int j = i;
+            while (j) {
+                if (j % 10 == k)
+                    ++count;
+                j /= 10;
+            }
+        }
+        return count;
+    }
+};
