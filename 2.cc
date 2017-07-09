ca3f452e20d74278d0e7aeb1d527f1bd005c0bf9
gaoxinge <gaoxx5@gmail.com>
2017-07-09 16:01:16 +0800

    2

--- 2.cc
+++ 2.cc
@@ -3,8 +3,8 @@ public:
     /*
      * @param n : description of n
      * @return: description of return
-	 * @solution: http://blog.csdn.net/qq_16255321/article/details/37994015
-	 */
+     * @solution: http://blog.csdn.net/qq_16255321/article/details/37994015
+     */
     long long trailingZeros(long long n) {
         if (n < 5) return 0;
         n /= 5;
