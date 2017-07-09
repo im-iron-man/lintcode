0a7bc517b96bc1fe82707900ee6bb863d314b3e9
gaoxinge <gaoxx5@gmail.com>
2017-07-09 15:54:59 +0800

    4

--- 4.cc
+++ 4.cc
@@ -1,8 +1,8 @@
 class Solution {
 public:
     /*
-     * @param n an integer
-     * @return the nth prime number as description.
+     * @param n: an integer
+     * @return: the nth prime number as description
      */
     int nthUglyNumber(int n) {
         int *p = new int[n];
