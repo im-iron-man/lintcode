15a8ed898fca2300b0145365d8ef726b46e2b31d
gaoxinge <gaoxx5@gmail.com>
2017-07-09 15:54:36 +0800

    2

--- 2.cc
+++ 2.cc
@@ -1,8 +1,10 @@
 class Solution {
 public:
-    // param n : description of n
-    // return: description of return
-	// http://blog.csdn.net/qq_16255321/article/details/37994015
+    /*
+     * @param n : description of n
+     * @return: description of return
+	 * @solution: http://blog.csdn.net/qq_16255321/article/details/37994015
+	 */
     long long trailingZeros(long long n) {
         if (n < 5) return 0;
         n /= 5;
